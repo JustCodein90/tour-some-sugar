eeb0c593d06faee0c4cfe64033a381b15b344d70
Justin Ireland <justinireland90@yahoo.com>
2023-09-23 21:21:46 -0400

    Added click Event to venues, Starting bands

--- scripts/bands.js
+++ scripts/bands.js
@@ -1,6 +1,28 @@
-import { getBands } from "./database.js";
+import { getBands, getBookings, getVenues } from "./database.js";
 
 const bands = getBands()
+const bookings = getBookings()
+const venues = getVenues()
+
+document.addEventListener(
+    click,
+    (clickEvent) => {
+        const itemClicked = clickEvent.target 
+
+        if (itemClicked.id.startsWith("band")) {
+            const [, bandId] = itemClicked.id.split("--")
+
+            for (const band of bands) {
+                if (band.id === parseInt(bandId)) {
+                    const assignment = filterVenues(band)
+                    const locationName = filterNames(assignment)
+                    window.alert(`Playing at ${locationName}`)
+
+                }
+            }
+        } 
+    }
+)
 
 
 export const Bands = () => {
--- scripts/venues.js
+++ scripts/venues.js
@@ -1,6 +1,58 @@
-import { getVenues } from "./database.js";
+import { getVenues, getBookings, getBands } from "./database.js";
 
 const venues = getVenues()
+const bookings = getBookings()
+const bands = getBands()
+
+document.addEventListener(
+    "click",  
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+
+        if (itemClicked.id.startsWith("venue")) {
+            const [,venueId] = itemClicked.id.split("--")
+
+            for (const venue of venues) {
+                
+                if (venue.id === parseInt(venueId)) {
+                   const assignments = filterBands(venue)
+                   const bandName = bookedBandNames(assignments)
+                   
+                   window.alert(` ${bandName} with be playing at ${venue.name}`)
+                }
+            }
+        }
+    }
+)
+
+
+const filterBands = (venue) => {
+    const assignments = []
+
+        for (const assignment of bookings) {
+            if (assignment.venueId === venue.id) {
+            assignments.push(assignment)
+        }
+    }
+
+    return assignments
+}
+
+
+const bookedBandNames = (assignments) => {
+    let bandNames = ""
+
+    for (const assignment of assignments) {
+
+        for (const band of bands) {
+            if (band.id === assignment.bandId) {
+                bandNames = `${bandNames} and ${band.name}`
+            }
+        }
+    }
+
+    return bandNames
+}
 
 
 export const Venues = () => {
